6a61550f5790a5e6ee5b86720e10f323d5381f58
Xraydylan <xraydylan@gmail.com>
2019-08-17 00:45:06 +0200

    Fixed Solver
    Fixed probelm in solver

--- modules/Solver/module.js
+++ modules/Solver/module.js
@@ -65,13 +65,44 @@ module.exports = class BestPony extends Module {
         var res = "";
         var alg = msg.content.split("multi");
         if (alg.length > 1 && alg[1] !== "") {
-            alg[1].split(",").forEach(item => res = this.do_clac(item));
+            alg = this.prepareMulti(alg[1].split(",")).forEach(item => res = this.do_clac(item));
             msg.channel.send(Tools.parseReply(this.config.simple_multi_solve, [msg.author, res]));
             Algebrite.clearall();
         } else {
             msg.channel.send(Tools.parseReply(this.config.solver_nothing, [msg.author]));
         }
         Application.modules.Discord.setMessageSent();
+    }
+
+    prepareMulti(pre) {
+        var data = [];
+        var append_string = "";
+        var append_status = 0;
+        for (var i = 0; i < pre.length; i++) {
+            var state = 0;
+            for (var a = 0; a < pre[i].length; a++) {
+                if (pre[i][a] === '(') {
+                    state++;
+                }
+                if (pre[i][a] === ')') {
+                    state--;
+                }
+            }
+            if (state === 0 && append_status === 0) {
+                data.push(pre[i])
+            } else if (state > 0) {
+                append_status = 1
+            } else if (state < 0) {
+                append_status = 0;
+                append_string += pre[i];
+                data.push(append_string);
+                append_string = "";
+            }
+            if (append_status === 1) {
+                append_string += pre[i] + ",";
+            }
+        }
+        return data;
     }
 
     do_clac(item) {
