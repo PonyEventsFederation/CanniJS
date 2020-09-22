e819c37fc4a70818274a5d81743dd6978e859896
ChrisFhey <chrisfhey@gmail.com>
2020-09-22 20:13:53 +0200

    Cleaned up Potato module

--- modules/Potato/module.js
+++ modules/Potato/module.js
@@ -14,26 +14,8 @@ module.exports = class Potato extends Module {
             Application.modules.Discord.client.on('message', (msg) => {
                 this.smartato_emo = Tools.getEmoji(Application.getClient(), 'smartato');
 
-                if (msg.author.bot) {
-                    return;
-                }
-
-                if (Application.modules.Discord.isUserBlocked(msg.author.id)) {
-                    return;
-                }
-
-                if (Application.modules.Discord.isMessageSent()) {
-                    return;
-                }
-
-                if (Tools.msg_contains_list(msg, this.config.phrase_potato)) {
-                    return this.potato(msg);
-                }
-                else if (Tools.msg_contains_list(msg, this.config.phrase_best_potato)) {
-                    return this.bestpotato(msg);
-                }
-                else if (Tools.msg_contains(msg, 'potato') || Tools.msg_contains(msg, 'smartato')) {
-                    return this.potatofy(msg);
+                if (Application.modules.Discord.checkUserAccess(msg.author)) {
+                    this.handle(msg);
                 }
             });
 
@@ -41,20 +23,22 @@ module.exports = class Potato extends Module {
         });
     }
 
-    bestpotato(msg) {
-        if (Application.modules.Discord.controlTalkedRecently(msg, this.config.bestpotatoType)) {
-            const random = Tools.getRandomIntFromInterval(0, this.config.ans_best_potato.length - 1);
-            msg.channel.send(Tools.parseReply(this.config.ans_best_potato[random], [msg.author])).then(sentEmbed => {
-                this.potatofy(sentEmbed);
-            });
-            Application.modules.Discord.setMessageSent();
+    handle(msg) {
+        if (Tools.msg_contains_list(msg, this.config.phrase_potato)) {
+            return this.potato(msg, this.config.potatoType, this.config.ans_potato);
+        }
+        else if (Tools.msg_contains_list(msg, this.config.phrase_best_potato)) {
+            return this.potato(msg, this.config.bestpotatoType, this.config.ans_best_potato);
+        }
+        else if (Tools.msg_contains(msg, 'potato') || Tools.msg_contains(msg, 'smartato')) {
+            return this.potatofy(msg);
         }
     }
 
-    potato(msg) {
-        if (Application.modules.Discord.controlTalkedRecently(msg, this.config.potatoType)) {
-            const random = Tools.getRandomIntFromInterval(0, this.config.ans_potato.length - 1);
-            msg.channel.send(Tools.parseReply(this.config.ans_potato[random], [msg.author])).then(sentEmbed => {
+    potato(msg, type, answerType) {
+        if (Application.modules.Discord.controlTalkedRecently(msg, type)) {
+            const random = Tools.getRandomIntFromInterval(0, answerType.length - 1);
+            msg.channel.send(Tools.parseReply(answerType[random], [msg.author])).then(sentEmbed => {
                 this.potatofy(sentEmbed);
             });
             Application.modules.Discord.setMessageSent();
