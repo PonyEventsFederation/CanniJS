57bcaa338401a1e96f5f4a151644dfba409af9ff
ChrisFhey <chrisfhey@gmail.com>
2020-09-23 00:23:26 +0200

    Added bapeth logic

--- modules/Bap/module.js
+++ modules/Bap/module.js
@@ -20,7 +20,9 @@ module.exports = class Boop extends Module {
             }
 
             Application.modules.Discord.client.on('message', (msg) => {
-                this.handle(msg);
+                if (Application.modules.Discord.checkUserAccess(msg.author)) {
+                    this.handle(msg);
+                }
             });
 
             return resolve(this);
@@ -28,43 +30,63 @@ module.exports = class Boop extends Module {
     }
 
     handle(msg) {
-        if (Application.modules.Discord.checkUserAccess(msg.author) && Tools.strStartsWord(msg.content, 'bap')) {
-            if (!msg.mentions.everyone && msg.mentions.users.array().length > 0) {
-                const users = msg.mentions.users.array();
+        if (!msg.mentions.everyone && msg.mentions.users.array().length > 0) {
+            if (Tools.strStartsWord(msg.content, 'bap')) {
+                this.processBaps(msg, this.config.bapType, 'bap');
+            }
 
-                if (users.length > this.config.bapLimit) {
-                    this.setCooldown(msg);
-                }
-
-                if (!Application.modules.Discord.hasCooldown(msg.author.id, this.config.bapType)) {
-                    this.processBaps(msg, users);
-                }
+            if (Tools.strStartsWord(msg.content, 'bapeth')) {
+                this.processBaps(msg, this.config.bapethType, 'bapeth');
             }
         }
     }
 
-    processBaps(msg, users) {
-        for (let i = 0; i < users.length; i++) {
-            if (Application.checkSelf(users[i].id)) {
-                this.selfBap(msg);
-                continue;
-            }
+    processBaps(msg, type, answerType) {
+        console.log(answerType);
+        const users = msg.mentions.users.array();
 
-            if (wachmann_id === users[i].id) {
-                this.wachmannBap(msg, users[i]);
-                continue;
-            }
-
-            this.bap(msg, users[i]);
+        if (users.length > this.config.bapLimit) {
+            this.setCooldown(msg);
         }
 
-        msg.delete();
+        if (!Application.modules.Discord.hasCooldown(msg.author.id, type)) {
+            for (let i = 0; i < users.length; i++) {
+                if (Application.checkSelf(users[i].id)) {
+                    const answers = this.getAnswerType('self' + answerType);
+                    this.selfBap(msg, answers);
+                    continue;
+                }
+
+                if (wachmann_id === users[i].id) {
+                    this.wachmannBap(msg, users[i]);
+                    continue;
+                }
+
+                const answers = this.getAnswerType(answerType);
+                this.bap(msg, users[i], answers);
+            }
+
+            msg.delete();
+        }
     }
 
-    bap(msg, user) {
-        const random = Tools.getRandomIntFromInterval(0, this.config.bapAnswer.length - 1);
+    getAnswerType(type) {
+        switch(type) {
+        case 'bap':
+            return this.config.bapAnswer;
+        case 'bapeth':
+            return this.config.bapethAnswer;
+        case 'selfbap':
+            return this.config.selfBapAnswer;
+        case 'selfbapeth':
+            return this.config.selfBapethAnswer;
+        }
+    }
 
-        msg.channel.send(Tools.parseReply(this.config.bapAnswer[random], [user])).then(message => {
+    bap(msg, user, answerType) {
+        const random = Tools.getRandomIntFromInterval(0, answerType.length - 1);
+
+        msg.channel.send(Tools.parseReply(answerType[random], [user])).then(message => {
             message.delete({ timeout: bapDeleteTimeout });
         });
 
@@ -72,7 +94,7 @@ module.exports = class Boop extends Module {
         Application.modules.Discord.setMessageSent();
     }
 
-    selfBap(msg) {
+    selfBap(msg, answerType) {
         let response;
 
         if (Tools.chancePercent(25)) {
@@ -83,8 +105,8 @@ module.exports = class Boop extends Module {
             ]));
         }
         else {
-            const random = Tools.getRandomIntFromInterval(0, this.config.canniBapAnswer.length - 1);
-            response = msg.channel.send(Tools.parseReply(this.config.canniBapAnswer[random], [
+            const random = Tools.getRandomIntFromInterval(0, answerType.length - 1);
+            response = msg.channel.send(Tools.parseReply(answerType[random], [
                 msg.author,
                 Application.modules.Discord.getEmoji('error'),
             ]));
