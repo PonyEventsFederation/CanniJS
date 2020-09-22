fe8ef6ee04a9c2f2a2cb063459d7457f6027c960
ChrisFhey <chrisfhey@gmail.com>
2020-09-22 19:00:11 +0200

    Cleaned up NoMessageProcessor module

--- modules/NoMessageProcessor/module.js
+++ modules/NoMessageProcessor/module.js
@@ -5,48 +5,41 @@ const Application = require('../../lib/Application');
 const Module = require('../../lib/Module');
 const Promise = require('bluebird');
 const Tools = require('../../lib/Tools');
+const remote_on = false;
+const remote_target = null;
 
 module.exports = class NoMessageProcessor extends Module {
     start() {
         return new Promise(resolve => {
             this.log.debug('Starting...');
 
-            this.remote_on = false;
-            this.remote_target = null;
-
             Application.modules.Discord.client.on('message', (msg) => {
-                if (msg.author.bot) {
-                    return;
-                }
-
-                if (Application.modules.Discord.isUserBlocked(msg.author.id)) {
-                    return;
-                }
-
-                if (Application.modules.Discord.isMessageSent()) {
-                    return;
-                }
-
-                // When no message was sent, Canni either says she doesn't understand, or boops someone at random if she's not mentioned.
-                if (msg.mentions.has(Application.getClient().user)) {
-                    if (!this.remote_on || this.remote_target !== msg.channel) {
-                        msg.channel.send(Tools.parseReply(this.config.stillLearningAnswer, [Application.modules.Discord.getEmoji('shy')]));
-                    }
-                }
-                else {
-                    const random = Tools.getRandomIntFromInterval(0, 300);
-                    if (random === 10) {
-                        msg.channel.send(Tools.parseReply(this.config.randomBoopAnswer, [msg.author]));
-                    }
-
-                    if (random === 42) {
-                        Application.modules.GamerCanni.letsPlay(msg, this.config.playGameAnswer);
-                    }
+                if (Application.modules.Discord.checkUserAccess(msg.author)) {
+                    this.handle(msg);
                 }
             });
 
             return resolve(this);
         });
+    }
+
+    handle(msg) {
+        // When no message was sent, Canni either says she doesn't understand, or boops someone at random if she's not mentioned.
+        if (msg.mentions.has(Application.getClient().user)) {
+            if (!remote_on || remote_target !== msg.channel) {
+                msg.channel.send(Tools.parseReply(this.config.stillLearningAnswer, [Application.modules.Discord.getEmoji('shy')]));
+            }
+        }
+        else {
+            const random = Tools.getRandomIntFromInterval(0, 500);
+            if (random === 10) {
+                msg.channel.send(Tools.parseReply(this.config.randomBoopAnswer, [msg.author]));
+            }
+
+            if (random === 42) {
+                Application.modules.GamerCanni.letsPlay(msg, this.config.playGameAnswer);
+            }
+        }
     }
 
     stop() {
