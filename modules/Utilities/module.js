66459f666274bd95f778b0552b2dab524a2be2c4
ChrisFhey <chrisfhey@gmail.com>
2020-09-22 21:18:11 +0200

    Cleaned up Utilities module

--- modules/Utilities/module.js
+++ modules/Utilities/module.js
@@ -11,65 +11,35 @@ module.exports = class Utilities extends Module {
         return new Promise(resolve => {
             this.log.debug('Starting...');
             Application.modules.Discord.client.on('message', (msg) => {
-                if (msg.author.bot) {
-                    return;
+                if (Application.modules.Discord.checkUserAccess(msg.author)) {
+                    this.handle(msg);
                 }
-
-                if (Application.modules.Discord.isUserBlocked(msg.author.id)) {
-                    return;
-                }
-
-                if (Application.modules.Discord.isMessageSent()) {
-                    return;
-                }
-
-                if (Tools.msg_contains_list(msg, this.config.phrase_bye_night)) {
-                    return this.bye_night(msg);
-                }
-                else if (Tools.msg_contains_list(msg, this.config.phrase_bye)) {
-                    return this.bye(msg);
-                }
-                else if (Tools.msg_contains_word_list(msg, this.config.phrase_hello)) {
-                    return this.hello(msg);
-                }
-                else if (Tools.msg_contains_list(msg, this.config.phrase_hello_morning)) {
-                    return this.hello_morning(msg);
-                }
-
             });
 
             return resolve(this);
         });
     }
 
-    bye_night(msg) {
-        if (Application.modules.Discord.controlTalkedRecently(msg, this.config.byenightType, false, 'channel', undefined, undefined, 90000)) {
-            const random = Tools.getRandomIntFromInterval(0, this.config.ans_bye_night.length - 1);
-            msg.channel.send(Tools.parseReply(this.config.ans_bye_night[random]));
-            Application.modules.Discord.setMessageSent();
+    handle(msg) {
+        if (Tools.msg_contains_list(msg, this.config.phrase_bye_night)) {
+            return this.sendMessage(msg, this.config.byenightType, this.config.ans_bye_night);
+        }
+        else if (Tools.msg_contains_list(msg, this.config.phrase_bye)) {
+            return this.sendMessage(msg, this.config.byeType, this.config.ans_bye);
+        }
+        else if (Tools.msg_contains_word_list(msg, this.config.phrase_hello)) {
+            return this.sendMessage(msg, this.config.helloType, this.config.ans_hello);
+        }
+        else if (Tools.msg_contains_list(msg, this.config.phrase_hello_morning)) {
+            return this.sendMessage(msg, this.config.hello_morning_Type, this.config.ans_hello_morning);
         }
     }
 
-    bye(msg) {
-        if (Application.modules.Discord.controlTalkedRecently(msg, this.config.byeType, false, 'channel', undefined, undefined, 90000)) {
-            const random = Tools.getRandomIntFromInterval(0, this.config.ans_bye.length - 1);
-            msg.channel.send(Tools.parseReply(this.config.ans_bye[random]));
-            Application.modules.Discord.setMessageSent();
-        }
-    }
+    sendMessage(msg, type, answerType) {
+        if (Application.modules.Discord.controlTalkedRecently(msg, type, false, 'channel', undefined, undefined, 90000)) {
+            const random = Tools.getRandomIntFromInterval(0, answerType.length - 1);
+            msg.channel.send(Tools.parseReply(answerType[random]));
 
-    hello(msg) {
-        if (Application.modules.Discord.controlTalkedRecently(msg, this.config.helloType, false, 'channel', undefined, undefined, 90000)) {
-            const random = Tools.getRandomIntFromInterval(0, this.config.ans_hello.length - 1);
-            msg.channel.send(Tools.parseReply(this.config.ans_hello[random]));
-            Application.modules.Discord.setMessageSent();
-        }
-    }
-
-    hello_morning(msg) {
-        if (Application.modules.Discord.controlTalkedRecently(msg, this.config.hello_morning_Type, false, 'channel', undefined, undefined, 90000)) {
-            const random = Tools.getRandomIntFromInterval(0, this.config.ans_hello_morning.length - 1);
-            msg.channel.send(Tools.parseReply(this.config.ans_hello_morning[random]));
             Application.modules.Discord.setMessageSent();
         }
     }
