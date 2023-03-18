e08f31ef73bcd499153b593a30bc0391e12451f1
Meadowsys <blazeykirin@gmail.com>
2023-03-18 14:01:51 -0700

    move old/new app into seperate fns, swap by comment/uncomment fn call

--- src/main.mjs
+++ src/main.mjs
@@ -1,80 +1,94 @@
+/* eslint-disable no-unused-vars */
+
+import "./lib/Module.mjs";
+
 import Application from "./lib/Application.mjs";
+import * as app from "./lib/Application.mjs";
 import events from "events";
 import path from "path";
 import url from "url";
 
-const stage = (process.env["STAGE"] || process.env["NODE_ENV"] || "dev").toLowerCase();
+// new_app();
+old_app();
 
-events.defaultMaxListeners = 50;
-
-if (stage == "dev") await import("dotenv/config");
-
-const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
-Application.configure({
-	rootDir: __dirname,
-	modules_path: __dirname + "/modules",
-	config_path: __dirname + "/config",
-	stage,
-	logLevelConsole: stage == "dev" ? "debug" : "info",
-	logLevelFile: stage == "dev" ? "info" : "info",
-	logLevelRemote: stage == "dev" ? "debug" : "info",
-	logformat: "DD.MM.YYYY HH:mm:ss",
-	logDir: __dirname + "/logs",
-	stages: [
-		"prod",
-		"dev"
-	]
-});
-
-// resources
-
-const modules = [
-	"Activity",
-	"Discord",
-	"Overload",
-	"Ignore",
-	"Holiday",
-	"Potato",
-	"UserJoined",
-	"Help",
-	"TimeToGalacon",
-	"Boop",
-	"Bap",
-	"Hug",
-	"Fanta",
-	"Bizaam",
-	"Assfart",
-	"BestPony",
-	"WorstPony",
-	"MentionCanni",
-	"DevCommands",
-	"Solver",
-	"GamerCanni",
-	"Greetings",
-	"Compliment",
-	"Hype",
-	"RoutineMessages",
-	"InterBotCom",
-	"NoMessageProcessor"
-];
-
-// remove all disabled modules
-const disabledModules = process.env["DISABLED_MODULES"] ? process.env["DISABLED_MODULES"].split(",").map(m => m.trim()) : [];
-disabledModules.forEach(module => {
-	const i = modules.indexOf(module);
-	i !== -1 && modules.splice(i, 1);
-});
-
-// add all enabled modules
-const enabledModules = process.env["ENABLED_MODULES"] ? process.env["ENABLED_MODULES"].split(",").map(m => m.trim()) : [];
-enabledModules.forEach(module => !modules.includes(module) && modules.push(module));
-
-for (const module of modules) {
-	await Application.registerModule(module);
+async function new_app() {
+	await app.start_app();
 }
 
-Application.run();
+async function old_app() {
+	const stage = (process.env["STAGE"] || process.env["NODE_ENV"] || "dev").toLowerCase();
 
-process.on("SIGINT", () => Application.stop());
-process.on("SIGTERM", () => Application.stop());
-process.on("exit", () => Application.stop());
+	events.defaultMaxListeners = 50;
+
+	if (stage == "dev") await import("dotenv/config");
+
+	const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+	Application.configure({
+		rootDir: __dirname,
+		modules_path: __dirname + "/modules",
+		config_path: __dirname + "/config",
+		stage,
+		logLevelConsole: stage == "dev" ? "debug" : "info",
+		logLevelFile: stage == "dev" ? "info" : "info",
+		logLevelRemote: stage == "dev" ? "debug" : "info",
+		logformat: "DD.MM.YYYY HH:mm:ss",
+		logDir: __dirname + "/logs",
+		stages: [
+			"prod",
+			"dev"
+		]
+	});
+
+	// resources
+
+	const modules = [
+		"Activity",
+		"Discord",
+		"Overload",
+		"Ignore",
+		"Holiday",
+		"Potato",
+		"UserJoined",
+		"Help",
+		"TimeToGalacon",
+		"Boop",
+		"Bap",
+		"Hug",
+		"Fanta",
+		"Bizaam",
+		"Assfart",
+		"BestPony",
+		"WorstPony",
+		"MentionCanni",
+		"DevCommands",
+		"Solver",
+		"GamerCanni",
+		"Greetings",
+		"Compliment",
+		"Hype",
+		"RoutineMessages",
+		"InterBotCom",
+		"NoMessageProcessor"
+	];
+
+	// remove all disabled modules
+	const disabledModules = process.env["DISABLED_MODULES"] ? process.env["DISABLED_MODULES"].split(",").map(m => m.trim()) : [];
+	disabledModules.forEach(module => {
+		const i = modules.indexOf(module);
+		i !== -1 && modules.splice(i, 1);
+	});
+
+	// add all enabled modules
+	const enabledModules = process.env["ENABLED_MODULES"] ? process.env["ENABLED_MODULES"].split(",").map(m => m.trim()) : [];
+	enabledModules.forEach(module => !modules.includes(module) && modules.push(module));
+
+	for (const module of modules) {
+		await Application.registerModule(module);
+	}
+
+	Application.run();
+
+	process.on("SIGINT", () => Application.stop());
+	process.on("SIGTERM", () => Application.stop());
+	process.on("exit", () => Application.stop());
+}
