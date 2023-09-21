24328388c99d44eabeb7e033b8b28c1760ee0854
Meadowsys <blazeykirin@gmail.com>
2023-09-21 10:02:37 -0700

    fix activity module and disable time to galacon

--- modules/Activity/module.js
+++ modules/Activity/module.js
@@ -7,14 +7,25 @@ const Promise = require("bluebird");
 const Tools = require("../../lib/Tools");
 const probability = 0.25;
 
+let okay_to_change = true;
+// 15 mins
+let timeout = setTimeout(() => okay_to_change = true, 15 * 60_000);
+
+function reset_wait() {
+	okay_to_change = false;
+	timeout.refresh();
+}
+
 module.exports = class Activity extends Module {
 	start() {
 		return new Promise(resolve => {
 			this.log.debug("Starting...");
-			this.activitySelect();
 
 			Application.modules.Discord.client.on("message", (msg) => {
 				this.randomizerActivity(msg);
+			});
+			Application.modules.Discord.client.on("ready", () => {
+				this.activitySelect();
 			});
 
 			return resolve(this);
@@ -22,23 +33,20 @@ module.exports = class Activity extends Module {
 	}
 
 	randomizerActivity(msg) {
-		if (Application.modules.Discord.checkUserAccess(msg.author) && Tools.chancePercent(probability, true)) {
+		if (Application.modules.Discord.checkUserAccess(msg.author) && okay_to_change && Tools.chancePercent(probability, true)) {
 			this.activitySelect();
 		}
 	}
 
 	activitySelect() {
-		if (!Application.modules.Discord.isReady()) {
-			return;
-		}
+		reset_wait();
+
 		const random = Tools.getRandomIntFromInterval(0, this.config.activity.length - 1);
-		Application.modules.Discord.client.user.setPresence({
+		const activity = this.config.activity[random];
+		Application.modules.Discord.client.user.setActivity(activity, {
 			status: "online",
-			afk: false,
-			activity: {
-				name: this.config.activity[random]
-			}
-		});
+			afk: false
+		}).then(() => this.log.info(`set activity to ${activity}`)).catch(console.error);
 	}
 
 
