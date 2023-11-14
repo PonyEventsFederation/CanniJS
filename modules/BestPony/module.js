18d4324e4eaf2d3a82071e7f43304f45dc04515a
Meadowsys <blazeykirin@gmail.com>
2023-11-13 20:04:14 -0800

    even more typings (...and some smol refactorings)
    hopefully the refactorings don't break anything lul, attempting to modify parseReply and getRandomIntFromInterval both broke like, immediately lol

--- modules/BestPony/module.js
+++ modules/BestPony/module.js
@@ -22,26 +22,48 @@ module.exports = class BestPony extends Module {
 		});
 	}
 
+	/**
+	 * @param { import("discord.js").Message } msg
+	 */
 	handle(msg) {
 		if (Tools.msg_contains(msg, "who is best pony")) {
-			this.whoIsBestPony(msg, this.config.bestPonyType, this.config.bestPonyAnswer1, "gc_cannibizaam");
-		} else if (Tools.msg_contains(msg, "canni is best pony") || Tools.msg_contains(msg, "canni soda is best pony")) {
-			this.whoIsBestPony(msg, this.config.canniBestPonyType, this.config.bestPonyAnswer2);
-		} else if (/b+i+z+a+m+ is best pony/i.test(msg.content)) {
-			this.whoIsBestPony(msg, this.config.bizaamBestPonyType, this.config.bestPonyAnswer3);
-		} else if (Tools.msg_contains(msg, "assfart is best pony")) {
-			this.whoIsBestPony(msg, this.config.assFartBestPonyType, this.config.bestPonyAnswer4);
-		} else if (Tools.msg_contains(msg, "fanta is best pony")) {
-			this.whoIsBestPony(msg, this.config.fantaBestPony, this.config.bestPonyAnswer5);
-		} else {
-			this.whoIsBestPony(msg, this.config.interjectType, this.config.bestPonyAnswerDefault);
+			return this.whoIsBestPony(msg, this.config.who, this.config.who_answer, "gc_cannibizaam");
 		}
+
+		if (Tools.msg_contains(msg, "canni is best pony") || Tools.msg_contains(msg, "canni soda is best pony")) {
+			return this.whoIsBestPony(msg, this.config.canni, this.config.canni_answer);
+		}
+
+		if (/b+i+z+a+m+ is best pony/i.test(msg.content)) {
+			return this.whoIsBestPony(msg, this.config.bizaam, this.config.bizaam_answer);
+		}
+
+		if (Tools.msg_contains(msg, "assfart is best pony")) {
+			return this.whoIsBestPony(msg, this.config.assfart, this.config.assfart_answer);
+		}
+
+		if (Tools.msg_contains(msg, "fanta is best pony")) {
+			return this.whoIsBestPony(msg, this.config.fanta, this.config.fanta_answer);
+		}
+
+		this.whoIsBestPony(msg, this.config.interject, this.config.interject_answer);
 	}
 
+	/**
+	 * @param { import("discord.js").Message } msg
+	 * @param { string } type
+	 * @param { Array<string> } answers
+	 * @param { string } emoji
+	 */
 	whoIsBestPony(msg, type, answers, emoji = "") {
 		if (Application.modules.Discord.controlTalkedRecently(msg, type)) {
 			const random = Tools.getRandomIntFromInterval(0, answers.length - 1);
-			msg.channel.send(Tools.parseReply(answers[random], [msg.author, Application.modules.Discord.getEmoji(emoji)]));
+
+			msg.channel.send(Tools.parseReply(
+				answers[random],
+				msg.author.toString(),
+				Application.modules.Discord.getEmoji(emoji).toString()
+			));
 
 			Application.modules.Discord.setMessageSent();
 		}
