cb795a20fdb51abc66cf9698ca61bc91aa630f82
Meadowsys <blazeykirin@gmail.com>
2023-11-16 23:24:21 -0800

    bunch ofmessy work, trying to get temporal to work but failing miserably

---
+++ ftl/TimeToGalacon.ftl
@@ -0,0 +1,1 @@
+status = Time to Galacon: { $days } days, { NUMBER($hours, minimumIntegerDigits: 2) }:{ NUMBER($minutes, minimumIntegerDigits: 2) } left! Hype!
--- modules/TimeToGalacon/module.js
+++ modules/TimeToGalacon/module.js
@@ -5,6 +5,7 @@ const Application = require("../../lib/Application");
 const Module = require("../../lib/Module");
 const moment = require("moment");
 const Tools = require("../../lib/Tools");
+const { Temporal } = require("@js-temporal/polyfill");
 
 // Set to false in case GalaCon is cancelled.
 const active = true;
@@ -59,12 +60,65 @@ module.exports = class TimeToGalacon extends Module {
 	}
 
 	setGalaconDate() {
-		this.galaconDate = !this.config.galaconDate ? moment() : moment(this.config.galaconDate);
+		this.galaconDate = moment(this.config.galaconDate);
 		this.log.info("Set galacon date to " + this.galaconDate.format());
 		this.galaconInterval = setInterval(() => this.updateGalaconDate(), (this.config.updateInterval || 10) * 1000);
 		this.updateGalaconDate();
 	}
 
+	set_galacon_date2() {
+		const galacon_tz = new Temporal.TimeZone(this.config.galacon_date.time_zone);
+		this.galacon_tz = galacon_tz;
+
+		this.calendar = new Temporal.Calendar("gregory");
+
+		const gc_date = Temporal.ZonedDateTime.from({
+			timeZone: galacon_tz,
+			calendar: this.calendar,
+			year: this.config.galacon_date.year,
+			month: this.config.galacon_date.month,
+			day: this.config.galacon_date.day,
+			hour: 0,
+			minute: 0
+		});
+		this.galacon_date_2 = gc_date;
+
+		this.galacon_date_2_instant = gc_date.toInstant();
+
+		this.log.info("Set galacon date to " + this.galacon_date_2.toString());
+
+		this.galaconInterval = setInterval(() => this.update_galacon_date2(), this.config.update_interval * 1000);
+		this.update_galacon_date2();
+	}
+
+	update_galacon_date2() {
+		// const until_galacon = Temporal.Now.instant()
+		// 	.toZonedDateTime({
+		// 		calendar: this.calendar,
+		// 		timeZone: this.galacon_tz
+		// 	})
+		// 	.until(this.galacon_date_2);
+
+		const until_galacon = Temporal.Now.instant()
+			.until(this.galacon_date_2_instant)
+			.round({ largestUnit: "day" });
+
+		const msg = this.t("status", {
+			days: until_galacon.days,
+			hours: until_galacon.hours % 24,
+			minutes: until_galacon.minutes
+		});
+
+		Application.modules.Discord.client.user.setPresence({
+			status: "online",
+			afk: false,
+			activity: { name: msg }
+		}).then().catch(console.error);
+	}
+
+	/**
+	 * @param { import("discord.js").Message } msg
+	 */
 	tellMeWhen(msg) {
 		const duration = this.getTimeRemaining();
 		const random = Tools.getRandomIntFromInterval(0, this.config.galaconAnswer.length - 1);
